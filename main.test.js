bead125eb730a0d7b1c33bd01637f8baecf13804
Long Phan <pvlong2000@gmail.com>
2022-12-08 09:24:33 +0400

    add tests from master for tree editing window

--- main.test.js
+++ main.test.js
@@ -1,10 +1,6 @@
-global.__DEV__ = true
-global.$ = require('jquery');
-global.d3 = require('d3');
-
 const puppeteer = require('puppeteer');
 
-describe('Opening viewTree.html', () => {
+describe('Upload tree content', () => {
   let browser;
   let page;
   beforeEach(async () => {
@@ -234,15 +230,193 @@ describe('Opening viewTree.html', () => {
   })
 });
 
-/*
-tests for editing tree functionality
+describe('Edit trees', () => {
+  let browser;
+  let page;
+  beforeEach(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();  
+    await page.goto("https://camel-lab.github.io/palmyra/viewtree.html"); // do this to be able to call functions in main.js, might need to direct the browser at this url
+  });
+  afterEach(() => {
+    browser.close();
+  });
 
-test("click on Listing button to show the sentences box", async () => {
-  const expectedNotDisplayValue = 'block';
-  const listingBtn = await page.$("input[type='button'][value='listing']");
-  await listingBtn.click();
-  const sentencesBoxDisplayValue = await page.$eval('#listing', el =>  getComputedStyle(el).getPropertyValue('display'));
-  expect(sentencesBoxDisplayValue).toEqual(expectedDisplayValue);
+  test('Add a new empty tree', async () => {
+    // simulate file uploads
+    let ConllFileUploader = await page.$('#inputFile');
+    await ConllFileUploader.uploadFile('palmyraSampleFiles/dataFiles/UD-English-Example.conllu');
+    let ConfigFileUploader = await page.$('#configFile');
+    await ConfigFileUploader.uploadFile('palmyraSampleFiles/config/ud.config');
+    // simulate upload button click
+    await page.click('#treebtn');
+
+    await page.$eval('[value="+ Tree"]', el => el.click());
+
+    let expectedTreeCount = 4;
+    let testingTreeCount = parseInt((await page.$eval('#currentTreeNumber', el => el.innerText)).trim().split('/')[1]);
+
+    let NodeElementArray = await page.$$('.node');
+
+    expect(testingTreeCount).toEqual(expectedTreeCount);
+    expect(NodeElementArray.length).toEqual(1);
+  });
+
+  test('Delete current tree when there is more than 1 tree', async () => {
+    // simulate file uploads
+    let ConllFileUploader = await page.$('#inputFile');
+    await ConllFileUploader.uploadFile('palmyraSampleFiles/dataFiles/UD-English-Example.conllu');
+    let ConfigFileUploader = await page.$('#configFile');
+    await ConfigFileUploader.uploadFile('palmyraSampleFiles/config/ud.config');
+    // simulate upload button click
+    await page.click('#treebtn');
+
+    // simulate deleting current tree
+    page.on("dialog", (dialog) => {
+      dialog.accept();
+    });
+    await page.$eval('[value="- Tree"]', el => el.click());
+
+    let expectedTreeCount = 2;
+    let testingTreeCount = parseInt((await page.$eval('#currentTreeNumber', el => el.innerText)).trim().split('/')[1]);
+
+    let expectedSent = "President Bush on Tuesday nominated two individuals to replace retiring jurists on federal courts in the Washington area .";
+    let testingSent = (await page.$eval('#sents', el => el.innerText)).trim();
+
+    expect(testingTreeCount).toEqual(expectedTreeCount);
+    expect(testingSent).toEqual(expectedSent);
+  });
+
+  test('Delete the last tree', async () => {
+    await page.$eval('#treedata2', el => el.value = "this is a sentence");
+    // simulate the click
+    await page.$eval('#treebtn2', el => el.click());
+
+    let testingMessage;
+    page.on("dialog", (dialog) => {
+      testingMessage = dialog.message();
+      dialog.accept();
+    });
+
+    // simulate deleting current tree
+    await page.$eval('[value="- Tree"]', el => el.click());
+
+    let expectedMessage = "Sorry! You cannot delete the last tree. Add a new tree first, then delete this tree.";
+
+    let expectedTreeCount = 1;
+    let testingTreeCount = parseInt((await page.$eval('#currentTreeNumber', el => el.innerText)).trim().split('/')[1]);
+
+    let expectedSent = "this is a sentence";
+    let testingSent = (await page.$eval('#sents', el => el.innerText)).trim();
+
+    expect(testingMessage).toEqual(expectedMessage);
+    expect(testingTreeCount).toEqual(expectedTreeCount);
+    expect(testingSent).toEqual(expectedSent);
+  });
+
+  test('Click Tags button first time if no config file uploaded', async () => {
+    await page.$eval('#treedata2', el => el.value = "this is a sentence");
+    // simulate the click
+    await page.$eval('#treebtn2', el => el.click());
+    await page.$eval('[value="tags"]', el => el.click());
+
+    let expectedLabelsText = "Relation Labels";
+    let testingLabelsText = (await page.$eval("#labels", el => el.innerText)).trim();
+
+    let expectedPosTagsText = "POS Tags";
+    let testingPosTagsText = (await page.$eval("#postags", el => el.innerText)).trim();
+
+    let expectedDisplayValue = 'block';
+    let testingLabelsSectionDisplayValue = await page.$eval('#labels', el =>  getComputedStyle(el).getPropertyValue('display'));
+    let testingPosTagsSectionDisplayValue = await page.$eval('#postags', el =>  getComputedStyle(el).getPropertyValue('display'));
+
+    // assert text
+    expect(testingLabelsText).toEqual(expectedLabelsText);
+    expect(testingPosTagsText).toEqual(expectedPosTagsText);
+    expect(testingLabelsSectionDisplayValue).toEqual(expectedDisplayValue);
+    expect(testingPosTagsSectionDisplayValue).toEqual(expectedDisplayValue);
+  });
+
+  test('Click Tags button first time if config file uploaded', async () => {
+    let ConfigFileUploader = await page.$('#configFile');
+    await ConfigFileUploader.uploadFile('palmyraSampleFiles/config/ud.config');
+    await page.$eval('#treedata2', el => el.value = "this is a sentence");
+    // simulate the click
+    await page.$eval('#treebtn2', el => el.click());
+    await page.$eval('[value="tags"]', el => el.click());
+
+    let expectedRelationLabelButtonCount = 37;
+    let testingRelationLabelButtonCount = (await page.$$('#labels > div > button')).length;
+
+    let expectedLabelsText = "Relation Labels\napposdislocatedexpliobjnsubjnmodnummodobjoblvocative\nadvclaclcsubjccompxcomp\nadvmodamoddiscourse\nauxcopclfcasedetmark\nconjcc\ncompoundfixedflat\nlistparataxis\ngoeswithorphanreparandum\ndeppunctroot"
+    let testingLabelsText = (await page.$eval("#labels", el => el.innerText)).trim();
+
+    let expectedPosTagButtonCount = 17;
+    let testingPosTagButtonCount = (await page.$$('#postags > div > button')).length;
+
+    let expectedPosTagsText = "POS Tags\nADJADVINTJNOUNPROPNVERB\nADPAUXCCONJDETNUMPARTPRONSCONJ\nPUNCTSYMX"
+    let testingPosTagsText = (await page.$eval("#postags", el => el.innerText)).trim();
+
+    let expectedDisplayValue = 'block';
+    let testingLabelsSectionDisplayValue = await page.$eval('#labels', el =>  getComputedStyle(el).getPropertyValue('display'));
+    let testingPosTagsSectionDisplayValue = await page.$eval('#postags', el =>  getComputedStyle(el).getPropertyValue('display'));
+
+     // assert text
+     expect(testingRelationLabelButtonCount).toEqual(expectedRelationLabelButtonCount);
+     expect(testingLabelsText).toEqual(expectedLabelsText);
+     expect(testingPosTagsText).toEqual(expectedPosTagsText);
+     expect(testingPosTagButtonCount).toEqual(expectedPosTagButtonCount);
+     expect(testingLabelsSectionDisplayValue).toEqual(expectedDisplayValue);
+     expect(testingPosTagsSectionDisplayValue).toEqual(expectedDisplayValue);
+  });
+
+  test('Click Tags button second time if config file uploaded', async () => {
+    let ConfigFileUploader = await page.$('#configFile');
+    await ConfigFileUploader.uploadFile('palmyraSampleFiles/config/ud.config');
+    await page.$eval('#treedata2', el => el.value = "this is a sentence");
+    // simulate the click
+    await page.$eval('#treebtn2', el => el.click());
+    await page.$eval('[value="tags"]', el => el.click());
+    await page.$eval('[value="tags"]', el => el.click());
+
+    let expectedDisplayValue = 'none';
+    let testingLabelsSectionDisplayValue = await page.$eval('#labels', el =>  getComputedStyle(el).getPropertyValue('display'));
+    let testingPosTagsSectionDisplayValue = await page.$eval('#postags', el =>  getComputedStyle(el).getPropertyValue('display'));
+
+    expect(testingLabelsSectionDisplayValue).toEqual(expectedDisplayValue);
+    expect(testingPosTagsSectionDisplayValue).toEqual(expectedDisplayValue);
+  });
+
+  test('Click Listing button if conll file has no #text line', async () => {
+    // simulate file uploads
+    let ConllFileUploader = await page.$('#inputFile');
+    await ConllFileUploader.uploadFile('palmyraSampleFiles/dataFiles/UD-English-Example.conllu');
+    // simulate upload button click
+    await page.click('#treebtn');
+    await page.click('[value="listing"]');
+
+    let expectedTextInListingDiv = "From the AP comes this story :\nPresident Bush on Tuesday nominated two individuals to replace retiring jurists on federal courts in the Washington area .\nBush nominated Jennifer M. Anderson for a 15 - year term as associate judge of the Superior Court of the District of Columbia , replacing Steffen W. Graae .";
+    let testingTextInListingDiv = (await page.$eval('#listing', el => el.innerText)).trim();
+
+    expect(testingTextInListingDiv).toEqual(expectedTextInListingDiv);
+  });
+
+  test('Click Listing button if conll file has #text line', async () => {
+    let ConllFileUploader = await page.$('#inputFile');
+    await ConllFileUploader.uploadFile('palmyraSampleFiles/dataFiles/UD-Arabic-Example.conllu');
+    // simulate upload button click
+    await page.click('#treebtn');
+    await page.click('[value="listing"]');
+
+    let expectedFirstSentInListingDiv = "لونغ بيتش (الولايات المتحدة) 15-7 (إف ب) - كل شيء تغير في حياة المتشرد ستيفن كنت عندما عثرت عليه شقيقته بعد عناء طويل لتبلغه بأنه ورث 300 ألف دولار وبأنه بات قادرا على وضع حد لعشرين سنة من حياة التشرد في شوارع مدينة لونغ بيتش في ولاية كاليفورنيا.";
+    let testingFirstSentInListingDiv = (await page.$eval('#listing', el => el.innerText)).trim();
+
+    // click on the first sentence
+    await page.$eval('[id="0"]', el => el.click());
+    let expectedCurrentTreeNumber = 1;
+    let testingCurrentTreeNumber = parseInt((await page.$eval('#currentTreeNumber', el => el.innerText)).trim().split('/')[0]);
+
+    expect(testingFirstSentInListingDiv).toEqual(expectedFirstSentInListingDiv);
+    expect(testingCurrentTreeNumber).toEqual(expectedCurrentTreeNumber);
+  })
 })
-
-*/
