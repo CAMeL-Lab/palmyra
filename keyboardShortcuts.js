316a99d78e9f8845ed44a6992587f385ec7ecf3e
Muhammed AbuOdeh <mra9407@nyu.edu>
2024-09-02 17:16:27 +0400

    fixed shortcuts, add morph and tag shortcuts

--- keyboardShortcuts.js
+++ keyboardShortcuts.js
@@ -8,12 +8,14 @@ window.addEventListener('keydown', function(event) {
   });
 
 window.addEventListener('keydown', function(event) {
-    if ((event.metaKey || event.ctrlKey) && event.shiftKey && event.key === 'l') {search(treesArray);}
+    if (event.ctrlKey && event.shiftKey && event.code == 'KeyL') {search(treesArray);}
   });
 
 // file menu shortcuts
 window.addEventListener('keydown', function(event) {
-  if ((event.metaKey || event.ctrlKey) && event.shiftKey && event.key === 's') {saveTreeRemote();}
-  else if ((event.metaKey || event.ctrlKey) && event.shiftKey && event.key === 'r') {renameToggle();}
-  else if ((event.metaKey || event.ctrlKey) && event.shiftKey && event.key === 'd') {downloadToggle();}
+  if (event.ctrlKey && event.shiftKey && event.code === 'KeyS') {saveTreeRemote();}
+  else if (event.ctrlKey && event.shiftKey && event.code === 'KeyR') {renameToggle();}
+  else if (event.ctrlKey && event.shiftKey && event.code === 'KeyD') {downloadToggle();}
+  else if (event.ctrlKey && event.shiftKey && event.code === 'KeyM') {editToggle();}
+  else if (event.ctrlKey && event.shiftKey && event.code === 'KeyT') {tagsToggle()}
 });
