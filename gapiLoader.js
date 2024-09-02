16a16459782f35e6313a2d6723365b0463baabfe
Muhammed AbuOdeh <mra9407@nyu.edu>
2024-09-02 18:10:36 +0400

    not setting mimetypes since text/ and application/ not working

--- gapiLoader.js
+++ gapiLoader.js
@@ -71,7 +71,7 @@ function showPicker(accessToken, callback) {
     .then((rsp) => {
       let credentials = rsp.data;
       let view = new google.picker.DocsView(google.picker.ViewId.DOCS).setParent('root').setIncludeFolders(true);
-      view.setMimeTypes("text/conllx, text/conllu");
+      // view.setMimeTypes("conllx, conllu");
       view.setMode(google.picker.DocsViewMode.LIST);
       let picker = new google.picker.PickerBuilder()
         .addView(view)
